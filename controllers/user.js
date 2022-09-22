a2f71aaa7af826c46fd8ec2ed2beb5fb86ff8515
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2022-09-22 12:17:44 +0530

    user validation

--- controllers/user.js
+++ controllers/user.js
@@ -2,6 +2,12 @@ const User = require("../models/user");
 exports.create = async (req, res) => {
   try {
     const { name, email, password } = req.body;
+    const oldUser = await User.findOne({ email });
+    if (oldUser) {
+      return res.json(400, {
+        error: "Email already in use",
+      });
+    }
     const newUser = new User({
       name,
       email,
--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/user");
+const { userValidator, validate } = require("../middlewares/validator");
 
-router.post("/create", userController.create);
+router.post("/create", userValidator, validate, userController.create);
 module.exports = router;
