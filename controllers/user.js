4068ed4b3c43d9b2dc6ea5f572822c1b790d58e1
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2022-09-22 11:48:31 +0530

    user model and create user route and change w.r.t port number

--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,19 @@
-exports.create = (req, res) => {
-  res.send("<h1>I will create later.</h1>");
+const User = require("../models/user");
+exports.create = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const newUser = new User({
+      name,
+      email,
+      password,
+    });
+    await newUser.save();
+    return res.json(200, {
+      user: newUser,
+    });
+  } catch (error) {
+    return res.json(500, {
+      message: error.message,
+    });
+  }
 };
--- server.js
+++ server.js
@@ -16,5 +16,5 @@ try {
 app.use(express.json());
 app.use("/api/users", require("./routes/user"));
 app.listen(8080, () => {
-  console.log("the port is listening on port 8000");
+  console.log("the port is listening on port 8080");
 });
