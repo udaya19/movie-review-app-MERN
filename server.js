7eb2de7e92ba6f5113ab7707d8abbece190812d1
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2022-09-22 10:56:19 +0530

    User route setup

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+app.use("/api/users", require("./routes/user"));
 app.listen(8080, () => {
   console.log("the port is listening on port 8000");
 });
