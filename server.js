091a73f7516ae7a884b9eed4b998e677b15b8f05
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2022-09-22 10:59:29 +0530

    connected to database

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const app = express();
-
+const mongoose = require("mongoose");
+try {
+  mongoose
+    .connect("mongodb://localhost/mern_moviereview")
+    .then(() => {
+      console.log("Database Connected Successfully");
+    })
+    .catch(() => {
+      console.log("Failed");
+    });
+} catch (ex) {
+  console.log(ex);
+}
 app.use(express.json());
 app.use("/api/users", require("./routes/user"));
 app.listen(8080, () => {
