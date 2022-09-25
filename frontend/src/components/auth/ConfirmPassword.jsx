2358ffba997ef78cab2ef3084449c94549541220
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2022-09-25 15:19:15 +0530

    Confirm password

--- frontend/src/components/auth/ConfirmPassword.jsx
+++ frontend/src/components/auth/ConfirmPassword.jsx
@@ -1,7 +1,30 @@
 import React from "react";
-
+import Title from "../form/Title";
+import FormInput from "../form/FormInput";
+import Submit from "../form/Submit";
 const ConfirmPassword = () => {
-  return <div>ConfirmPassword</div>;
+  return (
+    <div className="fixed inset-0 bg-primary text-white -z-10 flex justify-center items-center">
+      <div className="max-w-screen-xl max-auto ">
+        <form action="" className="bg-secondary rounded p-6 w-96 space-y-6">
+          <Title>Enter new password</Title>
+          <FormInput
+            label="Enter new password"
+            placeholder="*****"
+            name="password"
+            type="password"
+          />
+          <FormInput
+            label="Confirm new password"
+            placeholder="*****"
+            name="confirmPassword"
+            type="password"
+          />
+          <Submit value="Change Password" />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default ConfirmPassword;
