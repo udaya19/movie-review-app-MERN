8e88e03abb172a9bea0ef627c9de706f81f90a3a
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2022-10-04 08:40:23 +0530

    configuring cloudinary

--- backend/controllers/actor.js
+++ backend/controllers/actor.js
@@ -1,1 +1,13 @@
-exports.createActor = (req, res) => {};
+const Actor = require("../models/actor");
+const cloudinary = require("cloudinary").v2;
+cloudinary.config({
+  cloud_name: "dtoguorry",
+  api_key: "528411381656446",
+  api_secret: "hhESrXn2l4bVAaEtok_b2SXengQ",
+  secure: true,
+});
+exports.createActor = async (req, res) => {
+  const { name, about, gender } = req.body;
+  const { file } = req;
+  const newActor = new Actor({ name, about, gender });
+};
