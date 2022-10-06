e731b74a91abc2a3370b15c70052ba60a3d2f5fd
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2022-10-06 09:52:12 +0530

    Refractoring header to a seperate component

--- frontend/src/navigator/AdminNavigator.jsx
+++ frontend/src/navigator/AdminNavigator.jsx
@@ -5,12 +5,14 @@ import Movies from "../components/admin/Movies";
 import Actors from "../components/admin/Actors";
 import NotFound from "../components/NotFound";
 import AdminNavBar from "../components/admin/NavBar";
+import Header from "../components/admin/Header";
 
 const AdminNavigator = () => {
   return (
     <div className="flex">
       <AdminNavBar />
       <div className="flex-1 p-2 max-w-screen-xl">
+        <Header />
         <Routes>
           <Route path="/" element={<Dashboard />} />
           <Route path="/movies" element={<Movies />} />
