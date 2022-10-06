88c907814c8460258230ce19b12f86430f8f3ab5
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2022-10-06 08:50:33 +0530

    Admin navbar

--- frontend/src/navigator/AdminNavigator.jsx
+++ frontend/src/navigator/AdminNavigator.jsx
@@ -4,15 +4,21 @@ import Dashboard from "../components/admin/Dashboard";
 import Movies from "../components/admin/Movies";
 import Actors from "../components/admin/Actors";
 import NotFound from "../components/NotFound";
+import AdminNavBar from "../components/admin/NavBar";
 
 const AdminNavigator = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Dashboard />} />
-      <Route path="/movies" element={<Movies />} />
-      <Route path="/actors" element={<Actors />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <div className="flex">
+      <AdminNavBar />
+      <div className="flex-1 p-2 max-w-screen-xl">
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/actors" element={<Actors />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </div>
   );
 };
 
