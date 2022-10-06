13c11ab726f4b99861584bc2d0595675a8cc4991
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2022-10-06 09:23:03 +0530

    toggle options modal for create button

--- frontend/src/components/admin/Dashboard.jsx
+++ frontend/src/components/admin/Dashboard.jsx
@@ -1,7 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import { AiOutlinePlus } from "react-icons/ai";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const [showOptions, setShowOptions] = useState(false);
+  return (
+    <div className="flex items-center justify-between relative">
+      <input
+        type="text"
+        className="border-2 dark:border-dark-subtle border-light-subtle dark:focus:border-white focus:border-primary dark:text-white transition bg-transparent rounded text-lg p-1 outline-none"
+        placeholder="Search Movies..."
+      />
+      <button
+        onClick={() => {
+          setShowOptions(!showOptions);
+        }}
+        className="flex items-center space-x-2 border-secondary hover:border-primary text-secondary hover:opacity-80 transition font-semibold border-2 rounded text-lg px-3 py-1"
+      >
+        <span>Create</span>
+        <AiOutlinePlus />
+      </button>
+      <CreateOptions visible={showOptions} />
+    </div>
+  );
+};
+
+const CreateOptions = ({ visible }) => {
+  if (!visible) return null;
+  return (
+    <div className="absolute right-0 top-12 flex flex-col space-y-3 p-5 rounded dark:bg-secondary bg-white drop-shadow-lg">
+      <Options>Add Actor</Options>
+      <Options>Add Movie</Options>
+    </div>
+  );
+};
+
+const Options = ({ children, onClick }) => {
+  return (
+    <button
+      onClick={onClick}
+      className="dark:text-white text-secondary hover:opacity-80 transition"
+    >
+      {children}
+    </button>
+  );
 };
 
 export default Dashboard;
