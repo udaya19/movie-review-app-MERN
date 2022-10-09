21cca09f533d47902379d76c255d8ce900525176
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2022-10-09 12:03:01 +0530

    Refractoring search component and managing directors profile state

--- frontend/src/components/LiveSearch.jsx
+++ frontend/src/components/LiveSearch.jsx
@@ -1,123 +1,169 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState, forwardRef } from "react";
 import { commonInputClasses } from "../utils/theme";
 
-export const results = [
-  {
-    id: "1",
-    avatar:
-      "https://images.unsplash.com/photo-1643713303351-01f540054fd7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200&q=80",
-    name: "John Doe",
-  },
-  {
-    id: "2",
-    avatar:
-      "https://images.unsplash.com/photo-1643883135036-98ec2d9e50a1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200&q=80",
-    name: "Chandri Anggara",
-  },
-  {
-    id: "3",
-    avatar:
-      "https://images.unsplash.com/photo-1578342976795-062a1b744f37?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200&q=80",
-    name: "Amin RK",
-  },
-  {
-    id: "4",
-    avatar:
-      "https://images.unsplash.com/photo-1564227901-6b1d20bebe9d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200&q=80",
-    name: "Edward Howell",
-  },
-  {
-    id: "5",
-    avatar:
-      "https://images.unsplash.com/photo-1578342976795-062a1b744f37?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200&q=80",
-    name: "Amin RK",
-  },
-  {
-    id: "6",
-    avatar:
-      "https://images.unsplash.com/photo-1564227901-6b1d20bebe9d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200&q=80",
-    name: "Edward Howell",
-  },
-];
-
-const LiveSearch = () => {
-  let nextCount;
+export default function LiveSearch({
+  value = "",
+  placeholder = "",
+  results = [],
+  name,
+  resultContainerStyle,
+  selectedResultStyle,
+  inputStyle,
+  renderItem = null,
+  onChange = null,
+  onSelect = null,
+}) {
   const [displaySearch, setDisplaySearch] = useState(false);
-  const [focussedIndex, setFocussedIndex] = useState(-1);
+  const [focusedIndex, setFocusedIndex] = useState(-1);
+
   const handleOnFocus = () => {
     if (results.length) setDisplaySearch(true);
   };
-  const handleOnBlur = () => {
+
+  const closeSearch = () => {
     setDisplaySearch(false);
-    setFocussedIndex(-1);
+    setFocusedIndex(-1);
   };
+
+  const handleOnBlur = () => {
+    closeSearch();
+  };
+
+  const handleSelection = (selectedItem) => {
+    onSelect(selectedItem);
+  };
+
   const handleKeyDown = ({ key }) => {
+    let nextCount;
     const keys = ["ArrowDown", "ArrowUp", "Enter", "Escape"];
     if (!keys.includes(key)) return;
+
+    // move selection up and down
     if (key === "ArrowDown") {
-      nextCount = (focussedIndex + 1) % results.length;
+      nextCount = (focusedIndex + 1) % results.length;
     }
     if (key === "ArrowUp") {
-      nextCount = (focussedIndex + results.length - 1) % results.length;
+      nextCount = (focusedIndex + results.length - 1) % results.length;
     }
-    if (key === "Enter") return handleSelection(results[focussedIndex]);
-    setFocussedIndex(nextCount);
+
+    if (key === "Enter") return handleSelection(results[focusedIndex]);
+
+    setFocusedIndex(nextCount);
   };
-  const handleSelection = (selectedItem) => {
-    console.log(selectedItem);
+
+  const getInputStyle = () => {
+    return inputStyle
+      ? inputStyle
+      : commonInputClasses + " border-2 rounded p-1 text-lg";
   };
+
   return (
-    <div className="relative">
+    <div
+      tabIndex={1}
+      onKeyDown={handleKeyDown}
+      onBlur={handleOnBlur}
+      className="relative outline-none"
+    >
       <input
         type="text"
-        className={`border-2 rounded p-1 text-lg ${commonInputClasses}`}
-        placeholder="Search Profile"
+        id={name}
+        name={name}
+        className={getInputStyle()}
+        placeholder={placeholder}
         onFocus={handleOnFocus}
-        onBlur={handleOnBlur}
-        onKeyDown={handleKeyDown}
+        value={value}
+        onChange={onChange}
+        // onBlur={handleOnBlur}
+        // onKeyDown={handleKeyDown}
       />
       <SearchResults
-        onSelect={handleSelection}
-        focussedIndex={focussedIndex}
         results={results}
         visible={displaySearch}
+        focusedIndex={focusedIndex}
+        onSelect={handleSelection}
+        renderItem={renderItem}
+        resultContainerStyle={resultContainerStyle}
+        selectedResultStyle={selectedResultStyle}
       />
     </div>
   );
-};
+}
 
-const SearchResults = ({ visible, results = [], focussedIndex, onSelect }) => {
+// const renderItem = ({ id, name, avatar }) => {
+//   return (
+//     <div className="flex">
+//       <img src={avatar} alt="" />
+//       <p>{name}</p>
+//     </div>
+//   );
+// };
+
+const SearchResults = ({
+  visible,
+  results = [],
+  focusedIndex,
+  onSelect,
+  renderItem,
+  resultContainerStyle,
+  selectedResultStyle,
+}) => {
   const resultContainer = useRef();
+
   useEffect(() => {
     resultContainer.current?.scrollIntoView({
-      behaviour: "smooth",
+      behavior: "smooth",
       block: "center",
     });
-  }, [focussedIndex]);
+  }, [focusedIndex]);
+
   if (!visible) return null;
+
   return (
-    <div className="absolute right-0 left-0 top-10 bg-white dark:bg-secondary shadow-md p-2 max-h-64 space-y-2 mt-1 overflow-auto">
+    <div className="absolute right-0 left-0 top-10 bg-white dark:bg-secondary shadow-md p-2 max-h-64 space-y-2 mt-1 overflow-auto custom-scroll-bar">
       {results.map((result, index) => {
-        const { id, name, avatar } = result;
+        const getSelectedClass = () => {
+          return selectedResultStyle
+            ? selectedResultStyle
+            : "dark:bg-dark-subtle bg-light-subtle";
+        };
         return (
-          <div
-            onClick={() => onSelect(result)}
-            ref={index === focussedIndex ? resultContainer : null}
-            key={id}
-            className={
-              (index === focussedIndex
-                ? "dark:bg-dark-subtle bg-light-subtle"
-                : "") +
-              "cursor-pointer flex space-x-2 rounded overflow-hidden dark:hover:bg-dark-subtle hover:bg-light-subtle transition"
+          <ResultCard
+            key={result.id}
+            item={result}
+            renderItem={renderItem}
+            resultContainerStyle={resultContainerStyle}
+            selectedResultStyle={
+              index === focusedIndex ? getSelectedClass() : ""
             }
-          >
-            <img src={avatar} alt={name} className="w-16 h-16" />
-            <p className="dark:text-white font-semibold">{name}</p>
-          </div>
+            onMouseDown={() => onSelect(result)}
+          />
         );
       })}
     </div>
   );
 };
 
-export default LiveSearch;
+const ResultCard = forwardRef((props, ref) => {
+  const {
+    item,
+    renderItem,
+    resultContainerStyle,
+    selectedResultStyle,
+    onMouseDown,
+  } = props;
+
+  const getClasses = () => {
+    if (resultContainerStyle)
+      return resultContainerStyle + " " + selectedResultStyle;
+
+    return (
+      selectedResultStyle +
+      " cursor-pointer rounded overflow-hidden dark:hover:bg-dark-subtle hover:bg-light-subtle transition"
+    );
+  };
+  return (
+    <div onMouseDown={onMouseDown} ref={ref} className={getClasses()}>
+      {renderItem(item)}
+    </div>
+  );
+});
--- frontend/src/components/admin/MovieForm.jsx
+++ frontend/src/components/admin/MovieForm.jsx
@@ -1,8 +1,48 @@
 import React, { useState } from "react";
-import LiveSearch from "../LiveSearch";
-import TagsInput from "../TagsInput";
 import { commonInputClasses } from "../../utils/theme";
 import Submit from "../form/Submit";
+import LiveSearch from "../LiveSearch";
+import TagsInput from "../TagsInput";
+
+export const results = [
+  {
+    id: "1",
+    avatar:
+      "https://images.unsplash.com/photo-1643713303351-01f540054fd7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200&q=80",
+    name: "John Doe",
+  },
+  {
+    id: "2",
+    avatar:
+      "https://images.unsplash.com/photo-1643883135036-98ec2d9e50a1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200&q=80",
+    name: "Chandri Anggara",
+  },
+  {
+    id: "3",
+    avatar:
+      "https://images.unsplash.com/photo-1578342976795-062a1b744f37?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200&q=80",
+    name: "Amin RK",
+  },
+  {
+    id: "4",
+    avatar:
+      "https://images.unsplash.com/photo-1564227901-6b1d20bebe9d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200&q=80",
+    name: "Edward Howell",
+  },
+  {
+    id: "5",
+    avatar:
+      "https://images.unsplash.com/photo-1578342976795-062a1b744f37?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200&q=80",
+    name: "Amin RK",
+  },
+  {
+    id: "6",
+    avatar:
+      "https://images.unsplash.com/photo-1564227901-6b1d20bebe9d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200&q=80",
+    name: "Edward Howell",
+  },
+];
+
 const defaultMovieInfo = {
   title: "",
   storyLine: "",
@@ -10,64 +50,97 @@ const defaultMovieInfo = {
   cast: [],
   director: {},
   writers: [],
-  releaseDate: "",
+  releseDate: "",
   poster: null,
   genres: [],
   type: "",
   language: "",
   status: "",
 };
-const MovieForm = () => {
+
+export default function MovieForm() {
   const [movieInfo, setMovieInfo] = useState({ ...defaultMovieInfo });
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(movieInfo);
   };
+
+  const renderItem = (result) => {
+    return (
+      <div className="flex rounded overflow-hidden">
+        <img src={result.avatar} alt="" className="w-16 h-16 object-cover" />
+        <p className="dark:text-white font-semibold">{result.name}</p>
+      </div>
+    );
+  };
+
   const handleChange = ({ target }) => {
     const { value, name } = target;
+
     setMovieInfo({ ...movieInfo, [name]: value });
   };
+
   const updateTags = (tags) => {
     setMovieInfo({ ...movieInfo, tags });
   };
+
+  const updateDirector = (profile) => {
+    setMovieInfo({ ...movieInfo, director: profile });
+  };
+
   const { title, storyLine, director } = movieInfo;
+
   return (
-    <form action="" onSubmit={handleSubmit} className="flex space-x-3">
-      <div className="w-[70%] h-5 space-y-5">
+    <form onSubmit={handleSubmit} className="flex space-x-3">
+      <div className="w-[70%] space-y-5">
         <div>
           <Label htmlFor="title">Title</Label>
           <input
             id="title"
-            onChange={handleChange}
             value={title}
+            onChange={handleChange}
             name="title"
-            placeholder="Movie Title"
             type="text"
-            className={commonInputClasses + " border-b-2"}
+            className={commonInputClasses + " border-b-2 font-semibold text-xl"}
+            placeholder="Titanic"
           />
         </div>
+
         <div>
-          <Label htmlFor="storyLine">Story Line</Label>
+          <Label htmlFor="storyLine">Story line</Label>
           <textarea
             value={storyLine}
-            name="storyLine"
             onChange={handleChange}
+            name="storyLine"
             id="storyLine"
-            className={
-              commonInputClasses +
-              " resize-none border-b-2 dark:border-x-yellow-50 h-24"
-            }
-            placeholder="Movie Story"
+            className={commonInputClasses + " border-b-2 resize-none h-24"}
+            placeholder="Movie storyline..."
           ></textarea>
         </div>
-        <TagsInput onChange={updateTags} />
-        <LiveSearch />
+
+        <div>
+          <Label htmlFor="tags">Tags</Label>
+          <TagsInput name="tags" onChange={updateTags} />
+        </div>
+
+        <div className="">
+          <Label htmlFor="director">Director</Label>
+          <LiveSearch
+            name="director"
+            results={results}
+            placeholder="Search profile"
+            renderItem={renderItem}
+            onSelect={updateDirector}
+            value={director.name}
+          />
+        </div>
+
         <Submit value="Upload" />
       </div>
-      <div className="w-[30%] h-5 "></div>
+      <div className="w-[30%] h-5 bg-blue-400"></div>
     </form>
   );
-};
+}
 
 const Label = ({ children, htmlFor }) => {
   return (
@@ -79,5 +152,3 @@ const Label = ({ children, htmlFor }) => {
     </label>
   );
 };
-
-export default MovieForm;
